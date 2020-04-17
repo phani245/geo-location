f9dfef5edd6675e9572425c26af8e446f9b0075c
Phanindra Kanna <phakanna@publicisgroupe.net>
2020-04-17 11:33:48 +0530

    climate changes

--- geo-app/src/index.js
+++ geo-app/src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SeasonalDisplay from './SeasonalDisplay';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+    state = {
+        lat: null,
+        errMessage: ''
+    }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    componentDidMount(){
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({
+                lat: position.coords.latitude
+            }),
+            err => this.setState({
+                errMessage: err.message
+            })
+        );
+    }
+    render(){
+        if(this.state.errMessage && !this.state.lat){
+            console.log('sadsds');
+            return <div>Error: {this.state.errMessage}</div>;
+        }
+        if(!this.state.errMessage && this.state.lat){
+            return <SeasonalDisplay lat={this.state.lat}/>;  
+        }
+        return <div>Loading</div>;  
+    }
+}
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
