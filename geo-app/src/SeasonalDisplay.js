2b0b583470bccad955aa64d65c32b2fe4a80265d
Phanindra Kanna <phakanna@publicisgroupe.net>
2020-04-17 16:12:46 +0530

    destructuring the key values

--- geo-app/src/SeasonalDisplay.js
+++ geo-app/src/SeasonalDisplay.js
@@ -1,5 +1,15 @@
 import React from 'react';
 
+const seasonConfig = {
+    summer: {
+        text: 'Blur its Chilly',
+        iconName: 'snowflake'
+    },
+    winter: {
+        text: 'Lets hit the bleach',
+        iconName: 'sun'
+    }
+};
 const getSeason = (lat, month) => {
     if(month > 2 && month < 9) {
         return lat > 0 ? 'summer' : 'winter';
@@ -9,14 +19,13 @@ const getSeason = (lat, month) => {
 }
 const SeasonalDisplay = (props) => {
     const season = getSeason(props.lat, new Date().getMonth());
-    const text = season === 'winter' ? 'Burr its Chilly' : 'Lets hit the beach!';
-    const icon = season === 'winter' ? 'snowflake': 'sun';
+    const {text, iconName} = seasonConfig[season];
     
     return(
     <div>
-        <i className ={`${icon} icon`} />
+        <i className ={`${iconName} icon`} />
             <h1>{text}</h1>
-        <i className ={`${icon} icon`} />
+        <i className ={`${iconName} icon`} />
     </div>
     );
 }
